35ad4f4c85e1dcb4bd58aab965a9cec6c41407df
Muhammed nasser <mohamedabdelnasser37@gmail.com>
2023-02-25 16:25:53 +0200

    list all polls

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Poll
+from .serializers import PollSerializer
 
 
 def index(request):
-    return HttpResponse("Hello poll system")
+    serialized_polls = PollSerializer(Poll.objects.all(), many=True)
+    return JsonResponse({'polls': serialized_polls.data}, safe=False)
+
